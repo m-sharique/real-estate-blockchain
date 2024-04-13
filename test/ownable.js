819e9c51a5d7987d7c0efd6fde84938cec78389c
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-04-14 02:23:58 +0530

    Created Tests

---
+++ test/ownable.js
@@ -0,0 +1,13 @@
+const Ownable = artifacts.require("Ownable");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("Ownable", function (/* accounts */) {
+  it("should assert true", async function () {
+    await Ownable.deployed();
+    return assert.isTrue(true);
+  });
+});
